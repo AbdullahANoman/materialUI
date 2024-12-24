f5c4e410480ef6a83d99e592af2b60e62adf113a
Abdullah Al Noman <abdullahnoman4537@gmail.com>
2024-12-24 22:48:54 +0600

    learning mui ui , Muiselect multiple

--- src/components/MuiSelect.tsx
+++ src/components/MuiSelect.tsx
@@ -1,25 +1,55 @@
-import { Box, MenuItem, TextField } from "@mui/material";
+import { Box, MenuItem, Stack, TextField } from "@mui/material";
 import { useState } from "react";
 
 const MuiSelect = () => {
   const [country, setCountry] = useState("");
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setCountry(e.target.value as string);
+  const [countries, setCountries] = useState<string[] | string>([]);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCountry(event.target.value as string);
   };
-  console.log({country});
+
+  const handleCountriesValueChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const value = event.target.value;
+    setCountries(typeof value == "string" ? value.split(",") : value);
+  };
+
+  console.log({ countries, country });
   return (
-    <Box width="250px">
-      <TextField
-        label="Select Country"
-        select
-        value={country}
-        fullWidth
-        onChange={handleChange}
-      >
-        <MenuItem value="BA">Bangladesh</MenuItem>
-        <MenuItem value="IN">India</MenuItem>
-        <MenuItem value="AS">Australia</MenuItem>
-      </TextField>
+    <Box width="500px">
+      <Stack spacing={4} direction={"row"}>
+        <TextField
+          label="Select Country"
+          select
+          value={country}
+          fullWidth
+          onChange={handleChange}
+        >
+          <MenuItem value="BA">Bangladesh</MenuItem>
+          <MenuItem value="IN">India</MenuItem>
+          <MenuItem value="AS">Australia</MenuItem>
+        </TextField>
+        {/* multiple countries selection  */}
+        <TextField
+          label="Select Country"
+          select
+          value={countries}
+          fullWidth
+          onChange={handleCountriesValueChange}
+          SelectProps={{
+            multiple: true,
+          }}
+          size="small"
+          color="secondary"
+          helperText="Please select your country "
+        >
+          <MenuItem value="SW">Sweden</MenuItem>
+          <MenuItem value="NP">Nepal</MenuItem>
+          <MenuItem value="UAE">Dubai</MenuItem>
+        </TextField>
+      </Stack>
     </Box>
   );
 };
